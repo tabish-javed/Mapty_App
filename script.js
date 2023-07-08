8bb88a661801634c6628d2be7649ac66e6e4d5b7
TABISH <tabishjaved@hotmail.com>
2023-07-08 22:29:26 +0530

    fixed random ID generation

--- script.js
+++ script.js
@@ -11,9 +11,68 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    // id = (new Date() + '').slice(-10);
+    id = this.randomId();
+
+    constructor(coords, distance, duration) {
+        this.coords = coords;       // [lat, lng]
+        this.distance = distance;   // in Km
+        this.duration = duration;   // in Minutes
+    }
+
+    // Generate random ID
+    randomId (length = 10) {
+        return Math.random().toString(36).substring(2, length + 2);
+    }
+}
+
+//////////////////  RUNNING CLASS
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace () {
+        // min/km
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+
+}
+
+//////////////////  CYCLING CLASS
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.cadence = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed () {
+        // km/h
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+//////////////////  OBJECT INSTANTIATION
+// const running1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 524);
+
+// console.log(running1, cycling1);
+
+
+/////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
+
 class App {
     #map;
     #mapEvent;
+
     constructor() {
         this.#getPosition();
 
@@ -29,13 +88,13 @@ class App {
         //     });
 
         // FAKE GEOLOCATION
-        this.#loadMap()
+        this.#loadMap();
     }
 
     #loadMap (position) {
         // const { latitude, longitude } = position.coords;     // Enable for actual geolocation
 
-        const latitude = 12.8791619     // Delete when actual geolocation is enabled
+        const latitude = 12.8791619;     // Delete when actual geolocation is enabled
         const longitude = 77.6916485;    // Delete when actual geolocation is enabled
 
         const coords = [latitude, longitude];
